0608a677e0548f8d8a38fda514dc44a6657eafa7
hunter <hunteralzate@yahoo.com>
2026-06-29 13:44:58 -0700

    deleted old swim()

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -114,35 +114,6 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
     /**
      * Bubbles down the node currently at the given index.
      */
-    /*
-    private void sink(int index) {
-        // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
-        validateSinkSwimArg(index);
-        while (index < size) {
-            int minOfChildren = min(rightIndex(index), leftIndex(index));
-            if (minOfChildren == rightIndex(index)) {
-                int rightChildIndex = rightIndex(index);
-                if (min(index, rightChildIndex) == rightChildIndex) {
-                    swap(index, rightChildIndex);
-                    index = rightChildIndex;
-                    //rightChildIndex = rightIndex(rightChildIndex);
-                } else {
-                    break;
-                }
-            } else {
-                int leftChildIndex = leftIndex(index);
-                if (min(index, leftChildIndex) == leftChildIndex) {
-                    swap(index, leftChildIndex);
-                    index = leftChildIndex;
-                    //leftChildIndex = leftIndex(leftChildIndex);
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-     */
-
     private void sink(int index) {
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
